2f55b1467261b8494683a2ac4718c811f11e0b84
Joonhyuk Song <thenoface303@gmail.com>
2023-02-17 16:22:51 +0900

    cheeseburger: PE13: Set boot animation resolution

--- aosp_cheeseburger.mk
+++ aosp_cheeseburger.mk
@@ -14,6 +14,7 @@ $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 # Inherit some common aosp stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_NAME := aosp_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
